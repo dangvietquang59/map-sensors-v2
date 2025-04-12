d0aca3aa706671ee88f8c277ae9a2c3ab4ae7c39
Dang Viet Quang <dvquang5902@gmail.com>
2025-04-12 17:08:19 +0700

    feat: add logo

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -6,6 +6,8 @@ import SensorList from "@/components/sensor-list"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useTranslations } from "next-intl"
 import LanguageSelector from "@/components/LanguageSelector"
+import Image from "next/image"
+import svgs from "@/assets/svgs"
 
 export default function DashboardV2() {
   const t = useTranslations("sensor")
@@ -22,25 +24,29 @@ export default function DashboardV2() {
 
   return (
     <div className="container mx-auto p-4">
-     <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold mb-6">{t("title")}</h1>
-        <LanguageSelector/>
-     </div>
+        <h1 className="text-2xl font-bold">{t("title")}</h1>
 
-      <Tabs defaultValue="map" className="w-full">
-        <TabsList className="grid w-full max-w-md grid-cols-2">
-          <TabsTrigger value="map">{t("map")}</TabsTrigger>
-          <TabsTrigger value="list">{t("list")}</TabsTrigger>
-        </TabsList>
-        <TabsContent value="map" className="mt-4">
-          <div className="border rounded-lg overflow-hidden bg-white">
-            <SensorMapV2 ref={sensorMapRef} selectedSensorId={selectedSensorId} />
-          </div>
-        </TabsContent>
-        <TabsContent value="list" className="mt-4">
-          <SensorList onSelectSensor={handleSelectSensor} />
-        </TabsContent>
-      </Tabs>
+       <div className="flex items-center justify-center"> <Image src={svgs.logo} alt="logo" width={200} height={200}/></div>
+        <Tabs defaultValue="map" className="w-full">
+      <div className="flex items-center justify-between">
+          <TabsList className="grid w-full max-w-md grid-cols-2">
+            <TabsTrigger value="map">{t("map")}</TabsTrigger>
+            <TabsTrigger value="list">{t("list")}</TabsTrigger>
+          </TabsList>
+        <LanguageSelector/>
+
+      </div>
+
+          <TabsContent value="map" className="mt-4">
+            <div className="border rounded-lg overflow-hidden bg-white">
+              <SensorMapV2 ref={sensorMapRef} selectedSensorId={selectedSensorId} />
+            </div>
+          </TabsContent>
+          <TabsContent value="list" className="mt-4">
+            <SensorList onSelectSensor={handleSelectSensor} />
+          </TabsContent>
+        </Tabs>
+
     </div>
   )
 }
